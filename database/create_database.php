ea82a28fbee26e77e19639fe487248e03767c33d
Watermaker <vuibigboy@gmail.com>
2023-12-15 16:14:04 +0700

    fix url

--- database/create_database.php
+++ database/create_database.php
@@ -1,12 +1,5 @@
 <?php
-	$server = "localhost";//127.0.0.1; IP
-	$username = "root";
-	$password = "root";
-	$conn = mysqli_connect($server, $username, $password); //,$dbname
-	if(!$conn){
-		die("Kết nối không thành công: ".mysqli_connect_error());
-	}
-	echo "Kết nối thành công";
+	require '../connect.php';
 	$sql = "CREATE DATABASE IF NOT EXISTS WebProject";
 	if(mysqli_query($conn,$sql)){
 		echo "<br> Tạo thành công database";
