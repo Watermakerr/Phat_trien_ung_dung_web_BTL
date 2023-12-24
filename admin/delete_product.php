0bb9abe7fd67988b61334997153fb35341bd8f2d
Watermaker <vuibigboy@gmail.com>
2023-12-24 17:18:31 +0700

    handle checkbox

--- admin/delete_product.php
+++ admin/delete_product.php
@@ -4,13 +4,27 @@
 <body>
     <?php
     require 'header.php';
-    $id = $_GET['id'];
-    $sql = "DELETE FROM `products` WHERE product_id = $id";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: show_product.php");
-    } else {
-        echo "Error deleting record: " . $conn->error;
+    if (isset($_POST['checkbox'])) {
+        $ids = $_POST['checkbox'];
+        if (!empty($ids)) {
+            $ids = implode(',', array_map('intval', $ids)); // Sanitize the ids
+            $sql = "DELETE FROM `products` WHERE product_id IN ($ids)";
+            if ($conn->query($sql) !== TRUE) {
+                die("Error deleting record: " . $conn->error);
+            }
+            else{
+                header("Location: show_product.php");
+            }
+        }
     }
+    if (isset($_GET['id'])) {
+        $id = intval($_GET['id']); // Sanitize the id
+        $sql = "DELETE FROM `products` WHERE product_id = $id";
+        if ($conn->query($sql) !== TRUE) {
+            echo "Error deleting record: " . $conn->error;
+        }
+    }
+    
     ?>
 </body>
 </html>
--- admin/show_product.php
+++ admin/show_product.php
@@ -18,11 +18,11 @@
         </div>
     </div>
     <?php
-    $limit = 5;
+    $limit = 10;
     $page = isset($_GET['page']) ? $_GET['page'] : 1;
     $start = ($page - 1) * $limit;
 
-    $sql = "SELECT * FROM `products` order by `create_at` DESC LIMIT $start, $limit";
+    $sql = "SELECT `product_id`,products.name, `image`, `price`,`description`, categories.name as `catName`  FROM `products` INNER JOIN categories on categories.category_id = products.category_id order by products.create_at DESC LIMIT $start, $limit";
     $result = $conn->query($sql);
 
     $total_result = $conn->query("SELECT COUNT(*) as total FROM `products`");
@@ -30,40 +30,42 @@
     $total_pages = ceil($total_row['total'] / $limit);
 
     if ($result->num_rows > 0) { ?>
-        <table class='table table-bordered table-striped text-center'>
-            <thead class='thead-dark'>
-                <tr>
-                    <th></th>
-                    <th>Tên sản phẩm</th>
-                    <th>Ảnh</th>
-                    <th>Danh mục</th>
-                    <th>Giá thành</th>
-                    <th>Mô tả</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $counter = ($page - 1) * $limit + 1;
-                while ($row = $result->fetch_assoc()) { ?>
+        <form action="delete_product.php" method="post">
+            <table class='table table-bordered table-striped text-center'>
+                <thead class='thead-dark'>
                     <tr>
-                    <td><input type="checkbox" name="checkbox" value = "<?php $row['order_id']?>"></td>
-                        <td><?php echo $row['name'] ?></td>
-                        <td>
-                            <img class="card-img-top" style="width: 12rem;" src="asset/image/<?php echo $row['image']?>" alt="Card image cap">
-                        </td>    
-                        <td><?php echo $row['category_id'] ?></td>
-                        <td><?php echo $row['price'] ?></td>
-                        <td><?php echo $row['description'] ?></td>
-                        <td><a href='delete_product.php?id= <?php echo $row['product_id'] ?>' class='ml-1' onclick='return confirmDelete()'><i class='fas fa-trash-alt'></i></a>
-                        </td>
+                        <th></th>
+                        <th>Tên sản phẩm</th>
+                        <th>Ảnh</th>
+                        <th>Danh mục</th>
+                        <th>Giá thành</th>
+                        <th>Mô tả</th>
+                        <th></th>
                     </tr>
-                <?php
-                    $counter++;
-                }
-                ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php
+                    $counter = ($page - 1) * $limit + 1;
+                    while ($row = $result->fetch_assoc()) { ?>
+                        <tr>
+                        <td><input type="checkbox" name="checkbox[]" value="<?php echo $row['product_id']; ?>"></td>                            <td><?php echo $row['name'] ?></td>
+                            <td>
+                                <img class="card-img-top" style="width: 5rem;" src="../asset/image/<?php echo $row['image'] ?>" alt="Card image cap">
+                            </td>
+                            <td><?php echo $row['catName'] ?></td>
+                            <td><?php echo $row['price'] ?></td>
+                            <td><?php echo $row['description'] ?></td>
+                            <td><a href='delete_product.php?id= <?php echo $row['product_id'] ?>' class='ml-1' onclick='return confirmDelete()'><i class='fas fa-trash-alt'></i></a>
+                            </td>
+                        </tr>
+                    <?php
+                        $counter++;
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <input type="submit" name="delete" value="Delete Selected">
+        </form>
     <?php
     } else {
         echo "0 results";
@@ -79,4 +81,5 @@
         }
     </script>
 </body>
+
 </html>
