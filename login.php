9afc89f8ba4b844b860171f8bc33d642e78848db
Watermaker <vuibigboy@gmail.com>
2024-01-05 23:52:56 +0700

    add footer

--- login.php
+++ login.php
@@ -1,75 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require 'header.php';
+if (isset($_SESSION['username'])) {
+    header('Location: index.php');
+}
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="asset/css/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-</head>
-
-<body>
-    <?php
-    require 'header.php';
-    if (isset($_SESSION['username'])) {
-        header('Location: index.php');
-    }
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            if (password_verify($password, $row['password'])) {
-                $_SESSION['username'] = $row['username'];
-                $_SESSION['user_id'] = $row['user_id'];
-                $_SESSION['role_id'] = $row['role_id'];
-                if ($row['role_id'] == 1) {
-                    header('Location: admin/index.php');
-                } else {
-                    header('Location: index.php');
-                }
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['role_id'] = $row['role_id'];
+            if ($row['role_id'] == 1) {
+                header('Location: admin/index.php');
             } else {
-                $error = "Sai mật khẩu";
+                header('Location: index.php');
             }
         } else {
-            $error = "Sai tên đăng nhập";
+            $error = "Sai mật khẩu";
         }
-        mysqli_close($conn);
+    } else {
+        $error = "Sai tên đăng nhập";
     }
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 mx-auto my-5">
-                <form action="" method="POST">
-                    <?php
-                    if (isset($error)) {
-                        echo "<div class='alert alert-danger'>$error</div>";
-                    }
-                    ?>
-                    <h2 class="text-center">Trang đăng nhập</h2>
-                    <div class="form-group">
-                        <label for="username">Tên đăng nhập</label>
-                        <input type="username" name="username" id="username" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label for="password">Mật khẩu</label>
-                        <input type="password" name="password" id="password" class="form-control">
-                    </div>
-                    <input name="submit" type="submit" class="btn btn-primary form-control" id="submit" value="Đăng nhập"></input>
-                </form>
-                <p class="text-center">Bạn chưa có tài khoản? <a href="signup.php">Đăng ký</a></p>
-            </div>
+    mysqli_close($conn);
+}
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto my-5">
+            <form action="" method="POST">
+                <?php
+                if (isset($error)) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+                ?>
+                <h2 class="text-center">Trang đăng nhập</h2>
+                <div class="form-group">
+                    <label for="username">Tên đăng nhập</label>
+                    <input type="username" name="username" id="username" class="form-control">
+                </div>
+                <div class="form-group mb-3">
+                    <label for="password">Mật khẩu</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <input name="submit" type="submit" class="btn btn-primary form-control" id="submit" value="Đăng nhập"></input>
+            </form>
+            <p class="text-center">Bạn chưa có tài khoản? <a href="signup.php">Đăng ký</a></p>
         </div>
     </div>
-    </div>
-</body>
-
-</html>
+</div>
+</div>
+<?php
+    require 'footer.php';
+?>
--- signup.php
+++ signup.php
@@ -1,130 +1,117 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require 'header.php';
+if (isset($_SESSION['username'])) {
+    header('Location: index.php');
+}
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows == 0) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $fullname = $_POST['fullname'];
+        $email = $_POST['email'];
 
-<body>
-    <?php
-    require 'header.php';
-    if (isset($_SESSION['username'])) {
-        header('Location: index.php');
-    }
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->bind_param("s", $username);
+        $stmt = $conn->prepare("INSERT INTO users (username, password, fullname, email) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $username, $hashedPassword, $fullname, $email);
         $stmt->execute();
-        $result = $stmt->get_result();
-        if ($result->num_rows == 0) {
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-            $fullname = $_POST['fullname'];
-            $email = $_POST['email'];
 
-            $stmt = $conn->prepare("INSERT INTO users (username, password, fullname, email) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("ssss", $username, $hashedPassword, $fullname, $email);
-            $stmt->execute();
-
-            echo "<script>" .
-                "alert('Đăng ký thành công');" .
-                "window.location.href='login.php';" .
-                "</script>";
-        } else {
-            $error = "Tên đăng nhập đã tồn tại";
-        }
+        echo "<script>" .
+            "alert('Đăng ký thành công');" .
+            "window.location.href='login.php';" .
+            "</script>";
+    } else {
+        $error = "Tên đăng nhập đã tồn tại";
     }
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 mx-auto my-5">
-                <form action="" method="POST">
-                    <?php
-                    if (isset($error)) {
-                        echo "<div class='alert alert-danger'>$error</div>";
-                    }
-                    ?>
-                    <h2 class="text-center">Trang đăng ký</h2>
-                    <div class="form-group">
-                        <label for="name">Họ và tên</label>
-                        <input type="text" name="fullname" id="fullname" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="username">Tên đăng nhập</label>
-                        <input type="username" name="username" id="username" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Mật khẩu</label>
-                        <input type="password" name="password" id="password" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="rePassword">Nhập lại mật khẩu</label>
-                        <input type="password" name="rePassword" id="rePassword" class="form-control">
-                    </div>
-                    <input name="submit" type="submit" class="btn btn-primary" id="submit" value="Tạo tài khoản"></input>
-                </form>
-            </div>
+}
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-6 mx-auto my-5">
+            <form action="" method="POST">
+                <?php
+                if (isset($error)) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+                ?>
+                <h2 class="text-center">Trang đăng ký</h2>
+                <div class="form-group">
+                    <label for="name">Họ và tên</label>
+                    <input type="text" name="fullname" id="fullname" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="username">Tên đăng nhập</label>
+                    <input type="username" name="username" id="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Mật khẩu</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="rePassword">Nhập lại mật khẩu</label>
+                    <input type="password" name="rePassword" id="rePassword" class="form-control">
+                </div>
+                <input name="submit" type="submit" class="btn btn-primary" id="submit" value="Tạo tài khoản"></input>
+            </form>
         </div>
     </div>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var fullname = document.querySelector('#fullname');
-            var username = document.querySelector('#username');
-            var email = document.querySelector('#email');
-            var password = document.querySelector('#password');
-            var rePassword = document.querySelector('#rePassword');
-            var submitButton = document.querySelector('#submit');
+</div>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var fullname = document.querySelector('#fullname');
+        var username = document.querySelector('#username');
+        var email = document.querySelector('#email');
+        var password = document.querySelector('#password');
+        var rePassword = document.querySelector('#rePassword');
+        var submitButton = document.querySelector('#submit');
 
-            function checkInput() {
-                if (!fullname.value || !username.value || !password.value || !rePassword.value || !email.value) {
-                    submitButton.disabled = true;
-                } else {
-                    submitButton.disabled = false;
-                }
-                const passwordAlert = document.querySelector('.password-alert');
-                if (password.value != rePassword.value) {
-                    if (!passwordAlert) {
-                        var text = document.createElement('p');
-                        text.innerHTML = "Mật khẩu không khớp";
-                        text.classList.add('alert', 'alert-danger', 'mt-2', 'password-alert');
-                        rePassword.parentNode.appendChild(text);
-                    }
-                    submitButton.disabled = true;
-                } else if (passwordAlert) {
-                    passwordAlert.remove();
-                }
-                const usernameAlert = document.querySelector('.username-alert');
-                if (username.value.length < 5) {
-                    if (!usernameAlert) {
-                        var text = document.createElement('p');
-                        text.innerHTML = "Tên người dùng phải có ít nhất 5 ký tự";
-                        text.classList.add('alert', 'alert-danger', 'mt-2', 'username-alert');
-                        username.parentNode.appendChild(text);
-                    }
-                    submitButton.disabled = true;
-                } else if (usernameAlert) {
-                    usernameAlert.remove();
-                }
+        function checkInput() {
+            if (!fullname.value || !username.value || !password.value || !rePassword.value || !email.value) {
+                submitButton.disabled = true;
+            } else {
+                submitButton.disabled = false;
             }
-            fullname.addEventListener('input', checkInput);
-            username.addEventListener('input', checkInput);
-            email.addEventListener('input', checkInput);
-            password.addEventListener('input', checkInput);
-            rePassword.addEventListener('input', checkInput);
-            checkInput();
-        })
-    </script>
-
-</body>
-
-</html>
+            const passwordAlert = document.querySelector('.password-alert');
+            if (password.value != rePassword.value) {
+                if (!passwordAlert) {
+                    var text = document.createElement('p');
+                    text.innerHTML = "Mật khẩu không khớp";
+                    text.classList.add('alert', 'alert-danger', 'mt-2', 'password-alert');
+                    rePassword.parentNode.appendChild(text);
+                }
+                submitButton.disabled = true;
+            } else if (passwordAlert) {
+                passwordAlert.remove();
+            }
+            const usernameAlert = document.querySelector('.username-alert');
+            if (username.value.length < 5) {
+                if (!usernameAlert) {
+                    var text = document.createElement('p');
+                    text.innerHTML = "Tên người dùng phải có ít nhất 5 ký tự";
+                    text.classList.add('alert', 'alert-danger', 'mt-2', 'username-alert');
+                    username.parentNode.appendChild(text);
+                }
+                submitButton.disabled = true;
+            } else if (usernameAlert) {
+                usernameAlert.remove();
+            }
+        }
+        fullname.addEventListener('input', checkInput);
+        username.addEventListener('input', checkInput);
+        email.addEventListener('input', checkInput);
+        password.addEventListener('input', checkInput);
+        rePassword.addEventListener('input', checkInput);
+        checkInput();
+    })
+</script>
+<?php
+    require 'footer.php';
+?>
