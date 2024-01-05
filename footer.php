5584cf6d9957bf2fccb4f1997a693d84642c5954
Watermaker <vuibigboy@gmail.com>
2024-01-05 23:33:39 +0700

    add footer

--- footer.php
+++ footer.php
@@ -1,21 +1,28 @@
-<footer class="bg-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 border-right">
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
-            </div>
-            <div class="col-6">
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
-                <p class="text-center text-danger">Đây là footer</p>
+<footer class=" mt-3 bg-dark">
+        <div class="container w-50 p-5 float-left">
+            <h2 class="text-uppercase text-info">Công ty cổ phần điện thoại Mỏ địa chất</h2>
+            <p class="text-light"><i class="fa fa-phone"></i> Điện thoại: (+84-24) 3838 9633</p>
+            <p class="text-light"><i class="fa fa-map-marker"></i> Địa chỉ: Số 18 Phố Viên - Phường Đức Thắng - Q. Bắc Từ Liêm - Hà Nội</p>
+            <p class="text-light"><i class="fa fa-envelope"></i> Email: hanhchinhtonghop@humg.edu.vn</p>
+            <p class="text-light"><i class="fa fa-globe"></i> Website: www.humg.edu.vn</p>
+        </div>
+        <div class=" container pt-5 ">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14892.059279811194!2d105.773929!3d21.0720703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134552defbed8e9%3A0x1584f79c805eb017!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBN4buPIC0gxJDhu4thIGNo4bqldA!5e0!3m2!1svi!2s!4v1704472147585!5m2!1svi!2s"
+                width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="clearfix"></div>
+        <div class="mt-5 p-5 bg-dark text-white text-center border-top">
+            <p>Bản quyền thuộc về Công ty cổ phần điện thoại Mỏ địa chất</p>
+            <p>Đang Online: 5 | Tổng người online : 719311</p>
+            <div class="footer-link">
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-google"></i></a>
+                <a href=""><i class="fa fa-envelope"></i></a>
+                <a href=""><i class="fa fa-plus"></i></a>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 <?php 
 $conn->close();
 ?>
